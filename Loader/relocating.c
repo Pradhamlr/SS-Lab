d1039097b9cc904fce51cdf2a621575e95509222
Saquib Rizwan A S <147642522+Saquib-Rizwan@users.noreply.github.com>
2025-10-27 18:01:30 +0530

    Refactor relocating loader and add hex to binary conversion

--- Loader/relocating.c
+++ Loader/relocating.c
@@ -1,143 +1,106 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-void convert(char hex_mask[], char bit_mask[]);
-void display_file_content(const char *filename);
+void hex_to_binary(char hex[], char bin[]);
 
-int main(void) {
-    char add_str[7], length_str[7], input[10], bitmask_hex[13], pn[7];
-    char bitmask_bin[49];
-    int start_addr, text_addr, text_len, i, opcode, addr, actual_addr;
-    FILE *fp1, *fp2;
-
+int main()
+{
+    char line[100];
+    int actual_start;
+    FILE *fp = fopen("object_code.txt", "r");
+    
+    if(fp == NULL)
+    {
+        printf("File Open Failed");
+        return 1;
+    }
+    
     printf("Enter the actual starting address: ");
-    scanf("%x", &start_addr);
-
-    fp1 = fopen("RLIN.txt", "r");
-    if (fp1 == NULL) {
-        printf("Error: Cannot open RLIN.txt\n");
-        return 1;
-    }
-
-    fp2 = fopen("RLOUT.txt", "w");
-    if (fp2 == NULL) {
-        printf("Error: Cannot open RLOUT.txt\n");
-        fclose(fp1);
-        return 1;
-    }
-
-    // Safely read the first record
-    if (fscanf(fp1, " %s", input) == EOF) {
-        printf("Empty input file.\n");
-        return 1;
-    }
-
-    fprintf(fp2, "----------------------------\n");
-    fprintf(fp2, " ADDRESS   CONTENT\n");
-    fprintf(fp2, "----------------------------\n");
-
-    // Main loop for each record
-    while (!feof(fp1) && strcmp(input, "E") != 0) {
-        if (strcmp(input, "H") == 0) {
-            fscanf(fp1, "%s %s %s", pn, add_str, length_str);
-            fscanf(fp1, " %s", input); // move to next record
+    scanf("%x", &actual_start);
+    
+    while(fgets(line, sizeof(line), fp))
+    {
+        if(line[0] == 'H')
+        {
+            char prog[20], start[20], length[20];
+            sscanf(line, "H^%[^^]^%[^^]^%s", prog, start, length);
+            printf("Program name:%s ,Starting address:%s ,Length of Program:%s\n", prog, start, length);
         }
-
-        else if (strcmp(input, "T") == 0) {
-            fscanf(fp1, "%x", &text_addr);
-            fscanf(fp1, "%x", &text_len);
-            fscanf(fp1, "%s", bitmask_hex);
-
-            text_addr += start_addr;
-
-            convert(bitmask_hex, bitmask_bin);
-
-            int num_instructions = text_len / 3;
-
-            for (i = 0; i < num_instructions; i++) {
-                if (fscanf(fp1, "%x %x", &opcode, &addr) == EOF) {
-                    break;
+        else if(line[0] == 'T')
+        {
+            char start[10], length[20], bitmask[10], obj[100], binmask[40];
+            sscanf(line, "T^%[^^]^%[^^]^%[^^]^%[^\n]", start, length, bitmask, obj);
+            
+            int text_addr = (int)strtol(start, NULL, 16) + actual_start;
+            hex_to_binary(bitmask, binmask);
+            
+            printf("\nBitmask: %s -> Binary: %s\n", bitmask, binmask);
+            printf("Address\t\tObject Code\n");
+            
+            int i = 0, bit_index = 0;
+            int len = strlen(obj);
+            
+            while(i < len && obj[i] != '$')
+            {
+                if(obj[i] != '^')
+                {
+                    char instruction[7] = {0};
+                    strncpy(instruction, &obj[i], 6);
+                    
+                    int opcode, addr;
+                    sscanf(instruction, "%2x%4x", &opcode, &addr);
+                    
+                    if(binmask[bit_index] == '1')
+                        addr += actual_start;
+                    
+                    printf("%04X\t\t%02X%04X\n", text_addr, opcode, addr);
+                    
+                    text_addr += 3;
+                    bit_index++;
+                    i += 6;
                 }
-
-                if (bitmask_bin[i] == '1')
-                    actual_addr = addr + start_addr;
                 else
-                    actual_addr = addr;
-
-                fprintf(fp2, " %04X\t   %02X%04X\n", text_addr, opcode, actual_addr);
-                text_addr += 3;
-            }
-
-            if (fscanf(fp1, " %s", input) == EOF) {
-                break;
+                {
+                    i++;
+                }
             }
         }
-
-        else {
-            // If unknown record or formatting issue
-            if (fscanf(fp1, " %s", input) == EOF)
-                break;
+        else
+        {
+            printf("End of program\n");
         }
     }
-
-    fprintf(fp2, "----------------------------\n");
-
-    fclose(fp2);
-    fclose(fp1);
-
-    printf("\n Relocating loader finished.\n");
-
-    printf("\n\n--- Content of RLIN.txt ---\n");
-    display_file_content("RLIN.txt");
-
-    printf("\n\n--- Content of RLOUT.txt ---\n");
-    display_file_content("RLOUT.txt");
-    printf("\n");
-
+    
+    fclose(fp);
     return 0;
 }
 
-// Convert hex bitmask to binary string
-void convert(char hex_mask[], char bit_mask[]) {
+void hex_to_binary(char hex[], char bin[])
+{
     int i;
-    strcpy(bit_mask, "");
-
-    for (i = 0; i < strlen(hex_mask); i++) {
-        switch (hex_mask[i]) {
-            case '0': strcat(bit_mask, "0000"); break;
-            case '1': strcat(bit_mask, "0001"); break;
-            case '2': strcat(bit_mask, "0010"); break;
-            case '3': strcat(bit_mask, "0011"); break;
-            case '4': strcat(bit_mask, "0100"); break;
-            case '5': strcat(bit_mask, "0101"); break;
-            case '6': strcat(bit_mask, "0110"); break;
-            case '7': strcat(bit_mask, "0111"); break;
-            case '8': strcat(bit_mask, "1000"); break;
-            case '9': strcat(bit_mask, "1001"); break;
-            case 'A': case 'a': strcat(bit_mask, "1010"); break;
-            case 'B': case 'b': strcat(bit_mask, "1011"); break;
-            case 'C': case 'c': strcat(bit_mask, "1100"); break;
-            case 'D': case 'd': strcat(bit_mask, "1101"); break;
-            case 'E': case 'e': strcat(bit_mask, "1110"); break;
-            case 'F': case 'f': strcat(bit_mask, "1111"); break;
+    strcpy(bin, "");
+    
+    for(i = 0; i < strlen(hex); i++)
+    {
+        switch(hex[i])
+        {
+            case '0': strcat(bin, "0000"); break;
+            case '1': strcat(bin, "0001"); break;
+            case '2': strcat(bin, "0010"); break;
+            case '3': strcat(bin, "0011"); break;
+            case '4': strcat(bin, "0100"); break;
+            case '5': strcat(bin, "0101"); break;
+            case '6': strcat(bin, "0110"); break;
+            case '7': strcat(bin, "0111"); break;
+            case '8': strcat(bin, "1000"); break;
+            case '9': strcat(bin, "1001"); break;
+            case 'A': case 'a': strcat(bin, "1010"); break;
+            case 'B': case 'b': strcat(bin, "1011"); break;
+            case 'C': case 'c': strcat(bin, "1100"); break;
+            case 'D': case 'd': strcat(bin, "1101"); break;
+            case 'E': case 'e': strcat(bin, "1110"); break;
+            case 'F': case 'f': strcat(bin, "1111"); break;
         }
     }
-}
-
-// Function to display any file content
-void display_file_content(const char *filename) {
-    char line_buffer[256];
-    FILE *file_ptr = fopen(filename, "r");
-
-    if (file_ptr == NULL) {
-        printf("Error reading %s back.\n", filename);
-        return;
-    }
-
-    while (fgets(line_buffer, sizeof(line_buffer), file_ptr) != NULL) {
-        printf("%s", line_buffer);
-    }
-
-    fclose(file_ptr);
 }
